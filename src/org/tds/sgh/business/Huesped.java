1bc28b42cf800d913dd53700425f4255851bc761
Pablo Salgado <pablosalgadom@gmail.com>
2019-08-10 12:25:11 -0400

    agregue huesped

---
+++ src/org/tds/sgh/business/Huesped.java
@@ -0,0 +1,12 @@
+package org.tds.sgh.business;
+
+public class Huesped {
+	private String nombre;
+	private String documento;
+	
+	public Huesped(nombre, documento) {
+		this.nombre = nombre;
+		this.documento = documento;
+	}
+	
+}
